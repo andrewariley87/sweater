044d9fe8761e834cbd96cd04c6d58aac24a02571
Andrew Riley <andrew.riley@tangoe.com>
2021-03-25 18:58:10 -0400

    Adds Request to Get Forcast

--- lib/weather_request.rb
+++ lib/weather_request.rb
@@ -1,8 +1,15 @@
 require 'dotenv'
 Dotenv.load
+require 'net/http'
+require 'json'
 
 class WeatherRequest
-  def do_something
-    puts ENV['S3']
+  def get_forecast
+    uri    = URI("https://api.openweathermap.org/data/2.5/weather?q=indianapolis,in,usa&units=imperial&appid=#{ENV['WEATHERAPIKEY']}")
+    http                     = Net::HTTP.new(uri.host, uri.port)
+    request                  = Net::HTTP::Get.new(uri)
+    request['Content-Type']  = "application/json"
+    res = http.request(request)
+    puts JSON.parse(res.body)
   end
 end

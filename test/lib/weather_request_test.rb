09c6838ad9e96b1decaef24b3f3866cdbe9225b0
Andrew Riley <andrew.riley@tangoe.com>
2021-03-26 09:06:57 -0400

    CHORE: Adds Tests and Cleanup

--- test/lib/weather_request_test.rb
+++ test/lib/weather_request_test.rb
@@ -1,7 +1,24 @@
-require "minitest/autorun"
+require 'minitest/autorun'
+require 'mocha/minitest'
+require_relative '../../lib/weather_request'
 
 class WeatherRequestTest < Minitest::Test
-  def test_the_truth
-    assert true
+  def test_can_make_a_request
+    query         = 'kingston,jam'
+    uri           = URI("http://api.openweathermap.org/data/2.5/weather?q=#{query}&units=imperial&appid=#{ENV['WEATHERAPIKEY']}")
+    net_http_mock = mock
+    request_mock  = mock
+    body = {
+        'min_temp' => -5,
+        'max_temp' => 5
+    }.to_json
+    response      = OpenStruct.new({
+      body: body
+    })
+    Net::HTTP.expects(:new).with(uri.host, uri.port).returns(net_http_mock)
+    Net::HTTP::Get.expects(:new).with(uri).returns(request_mock)
+    net_http_mock.expects(:request).with(request_mock).returns(response)
+    request_mock.expects(:[]=).with('Content-Type', 'application/json')
+    WeatherRequest.new.get_forecast(query)
   end
 end
